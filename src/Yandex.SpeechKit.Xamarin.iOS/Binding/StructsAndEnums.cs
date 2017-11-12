f03b8c961dc32ba490a57d72856cc19e8141f3ba
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2017-11-12 19:23:54 +0300

    Add generated sources

---
+++ src/Yandex.SpeechKit.Xamarin.iOS/Binding/StructsAndEnums.cs
@@ -0,0 +1,30 @@
+namespace YandexSpeechKit
+{
+    public enum YSKLogLevel : uint
+    {
+        Error = 0,
+        Warn = 1,
+        Info = 2,
+        Debug = 3
+    }
+
+    public enum YSKSpeechErrorCode : uint
+    {
+        Ok = 0,
+        APIKey,
+        Recording,
+        Playing,
+        RecordingPermissions,
+        AudioInterrupted,
+        Encoding,
+        Network,
+        Server,
+        Cancelled,
+        NoSpeech,
+        NoTextToSynthesize,
+        NotAvailable,
+        Busy,
+        Unknown,
+        LanguageNotSupportedForModel
+    }
+}
