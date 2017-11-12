956b25b9881b9b4b4f717daf525832b466521944
Yauheni Pakala <evgeniy.pakalo@gmail.com>
2017-11-12 19:25:19 +0300

    Add sample project for iOS

---
+++ samples/YaSpeechKitApp.iOS/ViewController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+using UIKit;
+using YandexSpeechKit;
+
+namespace YaSpeechKitApp.iOS
+{
+    public partial class ViewController : UIViewController
+    {
+        private YSKSpeechRecognitionViewController _recognizerViewController;
+
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+
+            StartSpeechRecogrision();
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+
+
+        private void StartSpeechRecogrision()
+        {
+            _recognizerViewController = new YSKSpeechRecognitionViewController("ru-RU", "general");
+
+            PresentViewController(_recognizerViewController, true, () =>
+            {
+            
+            });
+
+            _recognizerViewController.Start();
+
+        }
+    }
+}
